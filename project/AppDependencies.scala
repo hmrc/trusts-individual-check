7a728a17cdf655547cc7e083e05a77ebb10dcbe5
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-03-04 11:45:58 +0000

    Updating dependencies.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,12 +5,12 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"        % "2.25.0",
-    "org.reactivemongo"       %% "play2-reactivemongo"              % "0.20.13-play27",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"        % "3.4.0",
+    "org.reactivemongo"       %% "play2-reactivemongo"              % "0.20.3-play27",
     "uk.gov.hmrc"             %% "domain"                           % "5.10.0-play-27",
-    "com.amazonaws"           % "aws-java-sdk-s3"                   % "1.11.820",
+    "com.amazonaws"            % "aws-java-sdk-s3"                  % "1.11.820",
     "com.enragedginger"       %% "akka-quartz-scheduler"            % "1.8.5-akka-2.6.x",
-    "uk.gov.hmrc"             %% "mongo-lock"                       % "6.23.0-play-27"
+    "uk.gov.hmrc"             %% "mongo-lock"                       % "6.24.0-play-27"
   )
 
   val test = Seq(
@@ -26,14 +26,15 @@ object AppDependencies {
 
   def apply(): Seq[ModuleID] = compile ++ test
 
-  val akkaVersion = "2.6.7"
-  val akkaHttpVersion = "10.1.12"
+  val akkaVersion = "2.6.12"
+  val akkaHttpVersion = "10.2.3"
 
   val overrides = Seq(
-    "com.typesafe.akka" %% "akka-stream" % akkaVersion,
-    "com.typesafe.akka" %% "akka-protobuf" % akkaVersion,
-    "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
-    "com.typesafe.akka" %% "akka-actor" % akkaVersion,
-    "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion
+    "com.typesafe.akka" %% "akka-stream_2.12" % akkaVersion,
+    "com.typesafe.akka" %% "akka-protobuf_2.12" % akkaVersion,
+    "com.typesafe.akka" %% "akka-slf4j_2.12" % akkaVersion,
+    "com.typesafe.akka" %% "akka-actor_2.12" % akkaVersion,
+    "com.typesafe.akka" %% "akka-http-core_2.12" % akkaHttpVersion,
+    "commons-codec" % "commons-codec" % "1.12"
   )
 }
