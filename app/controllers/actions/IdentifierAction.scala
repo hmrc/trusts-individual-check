1f42736a66772c5ea904b7bd10f5c268765477d5
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2021-05-12 10:36:12 +0100

    TRUS-4130 [CS] added session to request

--- app/controllers/actions/IdentifierAction.scala
+++ app/controllers/actions/IdentifierAction.scala
@@ -42,7 +42,7 @@ class AuthenticatedIdentifierAction @Inject()(override val authConnector: AuthCo
     val retrievals = Retrievals.internalId and
                      Retrievals.affinityGroup
 
-    implicit val hc : HeaderCarrier = HeaderCarrierConverter.fromRequest(request)
+    implicit val hc : HeaderCarrier = HeaderCarrierConverter.fromRequestAndSession(request, request.session)
 
     authorised().retrieve(retrievals) {
       case Some(internalId) ~ Some(Agent) =>
