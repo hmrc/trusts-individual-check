f0bd8c2b61a5073f76d7a0f4a519720d7e7938e7
Matthew Ward <96430760+matthewwardcap@users.noreply.github.com>
2022-09-27 14:14:40 +0100

    [DDCE-2694] Mongo-lock removed

--- it/suite/MongoSuite.scala
+++ it/suite/MongoSuite.scala
@@ -1,32 +1,41 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package suite
 
 import com.typesafe.config.ConfigFactory
-import org.scalatest._
-import play.api.Configuration
-import reactivemongo.api._
+import org.scalatest.time.{Millis, Seconds, Span}
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.{Application, Configuration}
+import uk.gov.hmrc.mongo.test.MongoSupport
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+trait MongoSuite extends MongoSupport {
 
-object MongoSuite {
+  implicit val defaultPatience: PatienceConfig = PatienceConfig(timeout = Span(30, Seconds), interval = Span(500, Millis))
 
   private lazy val config = Configuration(ConfigFactory.load(System.getProperty("config.resource")))
 
-  private lazy val parsedUri = Future.fromTry {
-    MongoConnection.parseURI(config.get[String]("mongodb.uri"))
+  val connectionString: String = config.get[String]("mongodb.uri")
+
+  def dropTheDatabase(): Unit = {
+    mongoDatabase.drop()
   }
 
-  lazy val connection =
-    parsedUri.map(MongoDriver().connection)
-}
+  lazy val createApplication: Application = new GuiceApplicationBuilder()
+    .configure(config)
+    .build()
 
-trait MongoSuite {
-  self: TestSuite =>
-
-  def database: Future[DefaultDB] =
-    for {
-      uri        <- MongoSuite.parsedUri
-      connection <- MongoSuite.connection
-      database   <- connection.database(uri.db.get)
-    } yield database
 }
